cdaad00e94a2bfd07a9715e7057bb078ba8cedf6
e-dinan <e-dinan@onu.edu>
2024-04-25 15:04:12 -0400

    Theme changes! Working light and dark mode radio button

--- app/src/main/java/com/theinnovationnation/skysurfer/ui/SettingsFragment.kt
+++ app/src/main/java/com/theinnovationnation/skysurfer/ui/SettingsFragment.kt
@@ -1,27 +1,55 @@
 package com.theinnovationnation.skysurfer.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.theinnovationnation.skysurfer.R
 
 
 class SettingsFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        // Find the RadioGroup by its ID
+        val themeRadioGroup = view.findViewById<RadioGroup>(R.id.themeRadioGroup)
+
+        // Find the RadioButtons by their IDs
+        val lightModeRadioButton = view.findViewById<RadioButton>(R.id.LightMode)
+        val darkModeRadioButton = view.findViewById<RadioButton>(R.id.DarkMode)
+
+        // Set an OnClickListener on each RadioButton
+        val onClickListener = View.OnClickListener { v ->
+            when (v.id) {
+                R.id.LightMode -> {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    }, 100) // Delay of 100 milliseconds
+                }
+                R.id.DarkMode -> {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }, 100) // Delay of 100 milliseconds
+                }
+            }
+        }
+
+        // Set the OnClickListener on each RadioButton
+        lightModeRadioButton.setOnClickListener(onClickListener)
+        darkModeRadioButton.setOnClickListener(onClickListener)
+
+        return view
     }
 
     companion object {
